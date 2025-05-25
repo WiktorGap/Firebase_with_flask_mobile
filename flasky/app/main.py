a0324173b467d6c9d7153431746db7e589feac5e
Your Name <you@example.com>
2025-05-25 15:10:43 +0200

    tralal

---
+++ flasky/app/main.py
@@ -0,0 +1,49 @@
+import firebase_admin
+from firebase_admin import credentials, db
+import json
+import os
+import pandas as pd
+
+from flask import Flask, render_template, request
+
+appRouting = Flask(__name__)
+
+cred = credentials.Certificate(r"C:/Users/admin/Desktop/myKey.json")
+app = firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://patientsdb-18075-default-rtdb.europe-west1.firebasedatabase.app/'
+})
+
+ref = db.reference("/Patient")
+
+
+@appRouting.route('/')
+def index():
+    return render_template("index.html")
+
+
+@appRouting.route('/retrive', methods = [ 'GET'])
+def retrive():
+    pacjenci = []
+    if request.method == 'GET':
+        ref = db.reference('/Patient')
+        dane = ref.get()
+        print(dane)
+
+        if dane:
+            for patient_id, patient_data in dane.items():
+                pacjenci.append({
+                    'id': patient_id,
+                    'name': patient_data.get('name'),
+                    'surname': patient_data.get('surname') or patient_data.get('surename'),
+                    'age': patient_data.get('age')
+                })
+        print(pacjenci)
+    return render_template('retrive.html', pacjenci= pacjenci)
+
+appRouting.route("/add", methods = ['POST'])
+def add():
+
+
+
+if __name__ == "__main__":
+    appRouting.run(debug=True)
